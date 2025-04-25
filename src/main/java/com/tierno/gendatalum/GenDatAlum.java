e7a826219a700d5e7743d5639760b359d384fc4c
Fulanito de Tal <fulanitodetal@ejemplo.com>
2025-04-25 18:15:40 +0200

    Primer commit del proyecto limpio

---
+++ src/main/java/com/tierno/gendatalum/GenDatAlum.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tierno.gendatalum;
+
+/**
+ *
+ * @author Manuel
+ */
+public class GenDatAlum {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
